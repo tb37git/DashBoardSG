d6691f61fd74a75aa0f7b31e6607b54485359fd3
tb37git <0qmpp0dd@duck.com>
2024-01-15 00:00:10 +0800

    Added 3-day forecast

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function fetchWeatherData(userLatitude, userLongitude) {
   try {
     const response = await fetch(API);
     const data = await response.json();
-    result["title"] = "Weather";
+    result["title"] = "Weather Now";
     const nearest = getNearest(userLatitude, userLongitude, data.area_metadata, "label_location");
     result["value"] = data.items[0].forecasts.find(area => area.area === nearest.name).forecast;
     result["location"] = nearest.name;
@@ -52,7 +52,45 @@ async function fetchWeatherData(userLatitude, userLongitude) {
     result["unit"] = "";  
   } catch (error) {
     console.error('Error fetching Weather data:', error);
-    // result["title"] = "Weather";
+    result["value"] = "Not available";
+  }
+  return result;
+}
+
+async function fetchWeatherToday(userLatitude, userLongitude) {
+  const DateTime = new Date().toISOString().slice(0, 19)+"Z";
+  const API = `https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date_time=${DateTime}`;
+  let result = {};
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    result["title"] = "Weather Today";
+    result["value"] = data.items[0].general.forecast;
+    result["location"] = null;
+    result["timeValid"] = null;
+    result["unit"] = null;
+  } catch (error) {
+    console.error('Error fetching Weather Today data:', error);
+    result["value"] = "Not available";
+  }
+  return result;
+}
+
+async function fetchWeatherForecast(index) {
+  const DateTime = new Date().toISOString().slice(0, 19)+"Z";
+  const API = `https://api.data.gov.sg/v1/environment/4-day-weather-forecast?date_time=${DateTime}`;
+  let result = {};
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+    result["title"] = `Weather: ${daysOfWeek[new Date().getDay() + index + 1]}`;
+    result["value"] = data.items[0].forecasts[index].forecast;
+    result["location"] = null;
+    result["timeValid"] = null;
+    result["unit"] = null;  
+  } catch (error) {
+    console.error('Error fetching Weather forecast data:', error);
     result["value"] = "Not available";
   }
   return result;
@@ -72,7 +110,27 @@ async function fetchTempData(userLatitude, userLongitude) {
     result["timeValid"] = data.items[0].timestamp.slice(11,16);
     result["unit"] = "°C";
  } catch (error) {
-    console.error('Error fetching Weather data:', error);
+    console.error('Error fetching Temp data:', error);
+    result["value"] = "Not available";
+  }
+  return result;
+}
+
+async function fetchTempForecast(index) {
+  const DateTime = new Date().toISOString().slice(0, 19)+"Z";
+  const API = `https://api.data.gov.sg/v1/environment/4-day-weather-forecast?date_time=${DateTime}`;
+  let result = {};
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+    result["title"] = `Temp: ${daysOfWeek[new Date().getDay() + index + 1]}`;
+    result["value"] = `${data.items[0].forecasts[index].temperature.low} - ${data.items[0].forecasts[index].temperature.high}`;
+    result["location"] = null;
+    result["timeValid"] = null;
+    result["unit"] = "°C";
+ } catch (error) {
+    console.error('Error fetching Temp forecast data:', error);
     result["value"] = "Not available";
   }
   return result;
@@ -92,7 +150,27 @@ async function fetchHumidityData(userLatitude, userLongitude) {
     result["timeValid"] = data.items[0].timestamp.slice(11,16);
     result["unit"] = "%";
  } catch (error) {
-    console.error('Error fetching Weather data:', error);
+    console.error('Error fetching Humidity data:', error);
+    result["value"] = "Not available";
+  }
+  return result;
+}
+
+async function fetchHumidityForecast(index) {
+  const DateTime = new Date().toISOString().slice(0, 19)+"Z";
+  const API = `https://api.data.gov.sg/v1/environment/4-day-weather-forecast?date_time=${DateTime}`;
+  let result = {};
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+    result["title"] = `Humidity: ${daysOfWeek[new Date().getDay() + index + 1]}`;
+    result["value"] = `${data.items[0].forecasts[index].relative_humidity.low} - ${data.items[0].forecasts[index].relative_humidity.high}`;
+    result["location"] = null;
+    result["timeValid"] = null;
+    result["unit"] = "%";
+ } catch (error) {
+    console.error('Error fetching Humidity forecast data:', error);
     result["value"] = "Not available";
   }
   return result;
@@ -199,13 +277,26 @@ app.post("/display", async (req, res) => {
   let result = {};
   
   if (req.body["Weather"]) {
-    result["Weather"] = await fetchWeatherData(userLatitude, userLongitude);
+    result["Weather Now"] = await fetchWeatherData(userLatitude, userLongitude);
+    // result["Weather Today"] = await fetchWeatherToday(userLatitude, userLongitude);
+    result["Weather 0"] = await fetchWeatherForecast(0);
+    result["Weather 1"] = await fetchWeatherForecast(1);
+    // result["Weather 2"] = await fetchWeatherForecast(2);
+    // result["Weather 3"] = await fetchWeatherForecast(3);
   };
   if (req.body["Temp"]) {
-    result["Temp"] = await fetchTempData(userLatitude, userLongitude);
+    result["Temp Now"] = await fetchTempData(userLatitude, userLongitude);
+    result["Temp 0"] = await fetchTempForecast(0);
+    result["Temp 1"] = await fetchTempForecast(1);
+    // result["Temp 2"] = await fetchTempForecast(2);
+    // result["Temp 3"] = await fetchTempForecast(3);
   };
   if (req.body["Humidity"]) {
-    result["Humidity"] = await fetchHumidityData(userLatitude, userLongitude);
+    result["Humidity Now"] = await fetchHumidityData(userLatitude, userLongitude);
+    result["Humidity 0"] = await fetchHumidityForecast(0);
+    result["Humidity 1"] = await fetchHumidityForecast(1);
+    // result["Humidity 2"] = await fetchHumidityForecast(2);
+    // result["Humidity 3"] = await fetchHumidityForecast(3);
   };
   if (req.body["Rainfall"]) {
     result["Rainfall"] = await fetchRainfallData(userLatitude, userLongitude);
